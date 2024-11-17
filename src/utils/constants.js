a02fb849bcf537dce40bd5daed84acbddf924ca9
Warier <felippemachado898@gmail.com>
2024-11-17 16:30:24 -0300

    feat: add constant

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const PLACEHOLDER_IMAGE = 'https://via.placeholder.com/200x200?text=No+Image';
+
+export const ERROR_MESSAGES = {
+    EMPTY_SEARCH: 'Por favor, insira um termo de busca',
+    API_ERROR: 'Erro ao buscar jogos. Tente novamente.',
+    INVALID_STEAM_ID: 'O Steam ID deve ser um número',
+    STORE_ERROR: 'Erro ao carregar lojas. Tente novamente.'
+};
+
+export const API_ENDPOINTS = {
+    GAMES: '/games',
+    STORES: '/stores'
+};
+
+export const SEARCH_TYPES = {
+    TITLE: 'title',
+    STEAM_ID: 'steamId'
+};
